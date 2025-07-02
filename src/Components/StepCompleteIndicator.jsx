22c740290eac7c917a5b225167db4698c4d208db
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-02 15:00:10 +0530

    add space

--- src/Components/StepCompleteIndicator.jsx
+++ src/Components/StepCompleteIndicator.jsx
@@ -5,9 +5,9 @@ function StepCompleteIndicator({step}) {
     return (
         <>
             <div className='numbers'>
-                <div className={step == 1? "active" : ""}>1</div>
-                <div className={step == 2? "active" : ""}>2</div>
-                <div className={step == 3? "active" : ""}>3</div>
+                <div className={step == 1 ? "active" : ""}>1</div>
+                <div className={step == 2 ? "active" : ""}>2</div>
+                <div className={step == 3 ? "active" : ""}>3</div>
             </div>
         </>
     )
