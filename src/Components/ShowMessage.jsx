0ecee173c4a9efd3516f6e3c61b55446b8db147c
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-02 11:12:10 +0530

    ShowMessage.jsx

---
+++ src/Components/ShowMessage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./index.css"
+function ShowMessage() {
+  return (
+    <>
+        <p className='message'>Hello</p>
+    </>
+  )
+}
+
+export default ShowMessage
