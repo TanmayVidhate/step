abc7204663b3fb0403b17994ce59b4f3e8b32b5d
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-02 11:11:46 +0530

    add DesignButton.jsx

---
+++ src/Components/DesignButton.jsx
@@ -0,0 +1,12 @@
+import "./index.css"
+function DesignButton({text}) {
+    return (
+        <>
+
+            <button style={{backgroundColor:"#7950f2",color:"#fff"}}>{text}</button>
+
+        </>
+    )
+}
+
+export default DesignButton;
