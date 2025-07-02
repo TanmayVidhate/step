f0160b84f0d643e01534988d4c1fde0d025af787
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-02 11:12:53 +0530

    add Divcontainer component in App.jsx

--- src/Views/App.jsx
+++ src/Views/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-
+import Divcontainer from '../Components/Divcontainer'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>HIII</h1>
+      <Divcontainer/>
     </>
   )
 }
