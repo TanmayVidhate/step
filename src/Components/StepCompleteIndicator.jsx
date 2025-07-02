b7ff231008178914c997d7c56ec056bfccaa8a49
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-02 11:11:10 +0530

    add StepCompleteIndicator.jsx

---
+++ src/Components/StepCompleteIndicator.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./index.css"
+function StepCompleteIndicator() {
+    return (
+        <>
+            <div className='numbers'>
+                <div className='active'>1</div>
+                <div>2</div>
+                <div>3</div>
+            </div>
+        </>
+    )
+}
+
+export default StepCompleteIndicator
